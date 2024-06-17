ef4783bcedca82e41272e92b92c6aedc155a7790
NaTheFunny <matiasbetorr@gmail.com>
2024-06-17 08:39:07 -0400

    se separo el array del index.js

--- index.js
+++ index.js
@@ -1,57 +1,24 @@
 
 const express = require("express");
 const multer = require('multer');
+const weapons = require('./weapons');
 const app = express();
 
 app.use(express.json());
+
 //Agregar Imagen por Local (No lo estoy ocupando ya que lo agrego por URL)
 const upload = multer({ dest: 'uploads/' });
-
-const weapons = [
-    {id: 1, 
-     name: 'Striker',
-     category: 'SMG',
-     image: 'https://assetsio.gnwcdn.com/mw3-striker.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp'},
-
-    {id: 2, 
-     name: 'AMR9',
-     category: 'SMG',
-     image: 'https://static.wikia.nocookie.net/callofduty/images/f/fa/AMR9_Gunsmith_MWIII.png/revision/latest?cb=20231007025105'},
-    
-    {id: 3, 
-     name: 'WSP9',
-     category: 'SMG',
-     image: 'https://www.charlieintel.com/cdn-cgi/image/width=3840,quality=60,format=auto/https://editors.charlieintel.com/wp-content/uploads/2023/11/14/Best-WSP-9-loadout-MW3-Modern-Warfare-3.jpg'},
-    
-    {id: 4, 
-     name: 'Rival-9',
-     category: 'SMG',
-     image: 'https://assetsio.gnwcdn.com/mw3-rival-9.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp'},
-
-    {id: 5, 
-     name: 'Striker-9',
-     category: 'SMG',
-     image: 'https://assetsio.gnwcdn.com/Striker-9.jpg?width=880&quality=80&format=jpg&auto=webp'},
-
-    {id: 6, 
-     name: 'WSP',
-     category: 'SMG',
-     image: 'https://www.dexerto.es/cdn-image/wp-content/uploads/sites/3/2024/03/08/wsp-swarm-cod-warzone-preview-1024x576-1.jpg?width=3840&quality=75&format=auto'},
-    
-    {id: 7, 
-     name: 'Superi-46',
-     category: 'SMG',
-     image: 'https://dotesports.com/wp-content/uploads/2024/05/mw3-best-superi-46-loadout-new-smg.jpg'},
-];
 
 //Obtener respuesta
 app.get('/',(req,res) =>{
     res.send('Node JS api');
 });
+
 //Obtener respuesta de armas
 app.get('/api/weapons/', (req,res) =>{
     res.send(weapons);
 })
+
 //Obtener arma por Id
 app.get('/api/weapons', (req, res) => {
     const { category, id } = req.query;
